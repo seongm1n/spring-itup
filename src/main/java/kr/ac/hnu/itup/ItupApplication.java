e9dd3c64f0fbf3fff1060d6738fa96dd8c0787c3
seongm1n <0411tjdals34@gmail.com>
2024-05-28 18:23:32 +0900

    init commit

---
+++ src/main/java/kr/ac/hnu/itup/ItupApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hnu.itup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItupApplication.class, args);
+    }
+
+}
